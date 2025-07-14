a944f1d5b457f70d3657ece0ab2ac5db2531f6d5
Fahd <fahdmo7amed76@gmail.com>
2025-07-14 21:36:38 +0300

    Add project files.

---
+++ Assigment 03/Program.cs
@@ -0,0 +1,584 @@
+﻿using System;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assigment_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Part 01
+
+
+            #region Q1
+
+            //Console.WriteLine("Enter Your Number : ");
+            //String? Num = Console.ReadLine();
+            //Console.WriteLine($"Your Number is: {Num}");
+            #endregion
+
+            #region Q2
+            //Console.WriteLine("Enter Your Number");
+            //int.TryParse(Console.ReadLine(), out int Number);
+            //Console.WriteLine($"Number = {Number}");
+            //The program will Convert String To Int And Convert  non-numeric characters To Defult Number = 0 
+
+
+            #endregion
+
+            #region Q3
+
+            //double a = 5 ,  b = 10;
+            //double result = a + b;
+            //Console.WriteLine($"{a} + {b} = {result}");
+            //The program will perform an arithmetic addition between two floating-point numbers using the double 
+            #endregion
+
+            #region Q4
+
+            //string text = "Hello Fahd";
+            //string sub = text.Substring(0, 5);
+            //Console.WriteLine(sub);
+
+
+            #endregion
+
+            #region Q5
+            //int X = 5;
+            //int Y = 10;
+            //Console.WriteLine(Y);
+            //Y = X;
+            //Console.WriteLine(Y);
+
+            // When you assign b = a, what happens is a copy of the value وThis means that a and b become independent variables
+            #endregion
+
+            #region Q6
+            //       Person P1 = new Person();
+            //        P1.Name = "Fahd";
+
+            //        Person P2 = P1;
+            //        P2.Name = "Mohamed";
+
+            //        Console.WriteLine(P1.Name);
+            //        Console.WriteLine(P2.Name);
+            //}
+            //// the Program Will Change address Of P2 And P2 = P1 , Cont change value Only 
+            #endregion
+
+            #region Q7
+
+            //string FNmame = "Fahd";
+            //string LNmame = "Mohamed";
+
+            //string FullName = FNmame +" "+ LNmame;
+            //Console.WriteLine($"Name : {FullName}");
+
+
+
+
+            #endregion
+
+            #region Q8
+
+            ////double  Interest, principal, rate, time;
+
+
+            //Console.WriteLine("Enter Your principal : ");
+            //double principal = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Your rate : ");
+            //double rate = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter Your time : ");
+            //double time = double.Parse(Console.ReadLine());
+
+            //double Interestt =(principal * rate * time) / 100;
+            //Console.WriteLine($"Interestt = {Interestt} ");
+
+            #endregion
+
+            #region Q9
+
+            //Console.WriteLine("Enter Your  weight in kilograms : ");
+            //double Weight = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Your height in meters : ");
+            //double Height = double.Parse(Console.ReadLine());
+
+            //double BMI = (Weight) / (Height * Height);
+            //Console.WriteLine($"BMI = {BMI}");
+
+
+            #endregion
+
+            #region Q10
+            // Console.WriteLine("Enter temperature");
+            //int  temperature =int.Parse(Console.ReadLine());
+
+            // if (temperature < 10)
+            // {
+            //     Console.WriteLine("Just Cold");
+            // }
+            // else if (temperature > 30)
+            // {
+            //     Console.WriteLine("Just Hot");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Just Good");
+            // }
+
+
+
+
+            #endregion
+
+            #region Q11
+
+            // Console.WriteLine("Enter date : Day =");
+            // int  Day = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Enter date : Month =");
+            // int  Month = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Enter date : Year =");
+            //int Year = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine($"Today's date : {Day} , {Month} , {Year}");
+            // Console.WriteLine($"Today's date : {Day} / {Month} / {Year}");
+            // Console.WriteLine($"Today's date : {Day} – {Month} – {Year}");
+
+
+
+
+
+
+
+
+
+            #endregion
+
+            #region Q12
+
+
+            //C = The event is on 06/14/2024 
+
+
+
+            #endregion
+
+            #region Q13
+
+            //f. A value 1 will be assigned to d.
+
+
+            #endregion
+
+
+            #region Q14
+            //  D.6 1
+
+            #endregion
+
+            #region Q15
+            // 7 7
+            #endregion
+
+            // Part 2
+
+            #region Q1
+
+            //Console.WriteLine("Enter Number ");
+            //int Num =int.Parse( Console.ReadLine());
+            //if (Num % 3 == 0 && Num % 4 == 0)
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //    Console.WriteLine("No");
+            #endregion
+
+            #region Q2
+
+            //Console.WriteLine("Enter Number ");
+            //int Number = int.Parse(Console.ReadLine());
+
+            //if (Number > 0)
+            //{
+            //    Console.WriteLine("positive");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("negative");
+
+            //}
+
+
+
+
+            #endregion
+
+            #region Q3
+
+            //Console.WriteLine("Enter Numbers ");
+            //int Num01 = int.Parse(Console.ReadLine());
+
+            //int Num03 = int.Parse(Console.ReadLine());
+
+            //int Num02 = int.Parse(Console.ReadLine());
+
+            //int Max = Math.Max(Num01, Math.Max(Num02,Num03));
+            //int Min = Math.Min(Num01, Math.Min(Num02, Num03));
+            //Console.WriteLine($"Max element = {Max}");
+            //Console.WriteLine($"Min element = {Min}");
+
+            #endregion
+
+            #region Q4
+
+
+            //Console.WriteLine("Enter Numbers ");
+            //int Num = int.Parse(Console.ReadLine());
+
+            //if (Num % 2 == 0)
+            //{
+            //    Console.WriteLine("Even");
+            //}
+            //else { Console.WriteLine("Odd"); }
+
+
+
+            #endregion
+
+            #region Q5
+
+
+            //Console.WriteLine("write character");
+            //string A = Console.ReadLine();
+            //switch (A)
+            //{
+            //    case "a":
+            //    case "e":
+            //    case "i":
+            //    case "o":
+            //    case "u":
+            //        Console.WriteLine("vowel");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Consonant");
+            //        break;
+            //}
+
+
+
+
+            #endregion
+
+            #region Q6
+            //Console.WriteLine("Enter Numbers ");
+            //int Num = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= Num ; i++) 
+            //{
+            //    Console.Write( i);
+            //}
+            #endregion
+
+            #region Q7
+
+            //Console.WriteLine("Enter Numbers ");
+            //int Num = int.Parse(Console.ReadLine());
+
+
+            //for (int i = 1; i <= 12; i++)
+            //{
+
+            //  Console.WriteLine(Num * i);
+            // }      
+
+
+
+
+
+
+
+            #endregion
+
+            #region Q8
+
+
+            //Console.WriteLine("Enter Numbers ");
+            //int Num = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < Num; i+=2) { Console.WriteLine(i); }
+
+
+            #endregion
+
+
+            #region Q9
+
+
+            //Console.WriteLine("Enter First number:");
+            //int Fnum = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter Last Number:");
+            //int LNum = Convert.ToInt32(Console.ReadLine());
+
+            //int power = 1;
+
+            //for (int i = 1; i <= LNum; i++)
+            //{
+            //    result = result * LNum;
+            //}
+
+            //Console.WriteLine(power );
+
+            #endregion
+
+
+            #region Q10
+
+
+
+            //int mark;
+            //int sub = 5;
+            //int total = 0;
+            //Console.WriteLine(" Enter Marks of five subjects:");
+
+            //for (int i = 0; i < sub; i++)
+            //{
+            //    total += int.Parse(Console.ReadLine());
+            //}
+
+            //double avg = total / sub;
+            //double Percentage = (total / 500.0) * 100;
+
+            //Console.WriteLine($"Total marks = {total}");
+            //Console.WriteLine($"Average Marks = {avg}");
+            //Console.WriteLine($"Percentage = {Percentage}");
+
+
+            #endregion
+
+            #region Q11
+
+            //Console.Write("Month Number: ");
+            //int Month = int.Parse(Console.ReadLine());
+
+            //int days = 0;
+            //switch (Month)
+            //{
+
+            //    case 1:
+            //    case 3:
+            //    case 5:
+            //    case 7:
+            //    case 8:
+            //    case 10:
+            //    case 12:
+            //        days = 31;
+            //        break;
+
+            //    case 4:
+            //    case 6:
+            //    case 9:
+            //    case 11:
+            //        days = 30;
+            //        break;
+            //    case 2:
+            //        days = 28;
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Invalid month");
+            //        return;
+
+            //}
+            //Console.WriteLine($"Days in Month :  {days}");
+
+
+
+
+
+
+            #endregion
+
+            #region Q12
+
+            //Console.Write("Enter First Nmber");
+            //int FNum = int.Parse(Console.ReadLine());
+            //Console.Write("Enter Last Number");
+            //int LNum = int.Parse(Console.ReadLine());
+
+
+            //Console.Write("Enter Operator (+, -, *, /, %): ");
+            //string opritors = Console.ReadLine();
+            //;
+            //switch (opritors)
+            //{
+            //    case "+":
+            //        Console.WriteLine($"{FNum} + {LNum} = {FNum + LNum}" );
+            //        break;
+            //    case "-":
+            //        Console.WriteLine($"{FNum} - {LNum} = {FNum - LNum}");
+            //        break;
+            //    case "/":
+            //        Console.WriteLine($"{FNum} / {LNum} = {FNum / LNum}");
+            //        break;
+            //        case "%":
+            //        Console.WriteLine($"{FNum}% {LNum} = {FNum % LNum}");
+            //        break;
+            //    case "*":
+            //        Console.WriteLine($"{FNum} * {LNum} = {FNum * LNum}");
+            //        break;
+            //    default:
+            //        Console.WriteLine("INVALID");
+            //        break;
+            //}
+
+
+
+
+
+
+
+
+            #endregion
+
+            #region Q13
+
+
+            //    Console.WriteLine("Enter string ");
+            //    string input = Console.ReadLine();
+
+            //    char[] chars= input.ToCharArray();
+            //    Array.Reverse(chars);
+
+
+            //string reversed = new string(chars);
+            //    Console.WriteLine("Reversed string: " + reversed);
+            #endregion
+
+            #region Q14
+
+            //    Console.WriteLine("Enter Number ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int reversed = 0 ;
+
+            //while (number != 0)
+            //{
+            //    int digit = number % 10;
+            //    reversed = reversed * 10 + digit;
+            //    number /= 10;
+            //}
+
+            //    Console.WriteLine("Reversed string: " + reversed);
+
+
+
+            #endregion
+
+            #region Q15
+
+
+            //Console.WriteLine("Test Data : \r\nInput starting number of range: ");
+            //int StartingNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Input ending number of range :  ");
+            //int EndingNumber = Convert.ToInt32(Console.ReadLine());
+
+            //for (int num = StartingNumber; num <= EndingNumber; num++)
+            //{
+            //    if (IsPrime(num))
+            //    {
+            //        Console.Write(num + " ");
+            //    }
+            //}
+            //static bool IsPrime(int number)
+            //{
+            //    if (number <= 1)
+            //        return false;
+
+            //    for (int i = 2; i <= Math.Sqrt(number); i++)
+            //    {
+            //        if (number % i == 0)
+            //            return false;
+            //    }
+
+            //    return true;
+            //}
+            #endregion
+
+            #region Q16
+
+
+            //Console.Write("Enter x1: ");
+            //int x1 = int.Parse(Console.ReadLine());
+            //Console.Write("Enter y1: ");
+            //int y1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter x2: ");
+            //int x2 = int.Parse(Console.ReadLine());
+            //Console.Write("Enter y2: ");
+            //int y2 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter x3: ");
+            //int x3 = int.Parse(Console.ReadLine());
+            //Console.Write("Enter y3: ");
+            //int y3 = int.Parse(Console.ReadLine());
+
+
+            //if ((x2 - x1) * (y3 - y2) == (x3 - x2) * (y2 - y1)) 
+            //{
+            //Console.WriteLine("The points lie on the same straight line");
+
+            //}else
+            //{ 
+            //Console.WriteLine("The points do NOT lie on the same straight line.");
+
+            //}
+
+
+            #endregion
+            #region Q17
+
+
+            //    Console.WriteLine("Enter the time taken by the worker");
+            //    double Hours =double.Parse(Console.ReadLine());
+
+            //    if (Hours >= 2 && Hours < 3)
+            //    {
+            //        Console.WriteLine("Highly efficient.");
+            //    }
+            //    else if (Hours >= 3 && Hours < 4)
+            //    {
+            //        Console.WriteLine("Work Need  To speed.");
+
+            //    }
+            //    else if (Hours >= 4 && Hours < 5)
+            //    {
+            //        Console.WriteLine("Worker should get training.");
+
+            //    }
+            //    else if (Hours >= 5)
+            //    {
+            //        Console.WriteLine("Worker should leave the company.");
+
+            //    }
+            //    else {
+            //        Console.WriteLine( "Invalid input ");
+            //    }
+
+
+            #endregion
+
+
+
+        }   
+    }
+}
+
+
---
+++ Session2/Program.cs
@@ -0,0 +1,363 @@
+﻿using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Session2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region stirng Formatatting
+            // stirng Formatatting
+            // Equation : X+ Y = 12 
+
+
+            //  int X = 3; int Y= 4;
+            //////String Concatnation 
+            ////string Message = "Equation : " + X + " + " + Y + " = " + (X + Y);
+            ////Console.WriteLine(Message);
+            ////String : Immutable -> Cant Change its Value After Creation 
+
+            //// 2 . String Format 
+            //string Message = string.Format("Equation: {0} + {1} = {2}", X , Y, X+Y);
+            //Console.WriteLine(Message);
+
+            // 3. String Intepolution 
+            //$ : 
+            //string Message = $"Equation : {X} + {Y} = {X+Y}";
+            //Console.WriteLine(Message);
+            #endregion
+
+
+            #region Control Statements
+
+
+            // Control Statements
+            // Control Statements : 1. Conditional Control Statements (if statement , Switch Case)
+
+            // Control Statements : 2. Loop Control Statements (For , Foreach , While , Do While )
+            //Console.WriteLine("hello");
+            //Console.WriteLine("hello");
+            //Console.WriteLine("hello");
+            //Console.WriteLine("hello");
+
+
+
+
+            // Control Statements : 1. Conditional Control Statements (if statement , Switch Case)
+            //Console.WriteLine("Enter Number Of Month in The 1st Quarter ");
+            //int MonthNumber = int.Parse(Console.ReadLine());
+            // 1 -- > Jan
+            // 2 -- > Feb 
+            // 2 --> Mar
+
+
+            //Console.WriteLine( "Jan" );
+            //Console.WriteLine("Feb");
+            //Console.WriteLine("Mar");
+            //if (/*Expession*/) 
+            //{
+            //        // Code 
+            //        // This Code Will Be Executed When The Condition Is True
+            //}
+            //1. Using if
+            //if (MonthNumber == 1) 
+            //{
+            //    Console.WriteLine( "Jan" );
+
+            //}
+            // if (MonthNumber == 2) 
+            //{
+            //    Console.WriteLine("Feb");
+            //}
+            //if (MonthNumber == 3 )
+            //{
+            //    Console.WriteLine("Mar ");
+            //}
+
+            //if (MonthNumber == 1)
+            //{
+            //    Console.WriteLine("Jan");
+
+            //}
+            //else if (MonthNumber == 2)
+            //{
+            //    Console.WriteLine("Feb");
+            //}
+            //else if (MonthNumber == 3)
+            //{
+            //    Console.WriteLine("Mar ");
+            //}
+            //else
+            //{ Console.WriteLine("Invaled Month Number "); }
+
+
+            //2. Using Switch
+            //switch (/*Expression*/)
+            //{
+
+            //    case 0:
+            //}
+            //Console.WriteLine("Enter Number Of Month in The 1st Quarter ");
+            //int MonthNumber = int.Parse(Console.ReadLine());
+
+            ////Jump Table 
+            //switch (MonthNumber)
+            //{
+            //    case 1 :
+            //        Console.WriteLine("Jan");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Feb");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Mar");
+            //       break ; 
+            //        default: Console.WriteLine("Invaled Month Number ");
+            //        break;
+            //}
+
+
+            #endregion
+
+            #region EX01
+
+            //Console.WriteLine("Enter Your Name : ");
+            //string NAme = Console.ReadLine();
+            //// Hi Ahmed 
+            //if (NAme == "Ahmed " || NAme == "ahmed")
+            //{
+            //    Console.WriteLine("Hi Ahmed ");
+            //}
+
+            //Console.WriteLine("Enter Your Name : ");
+            //string NAme = Console.ReadLine();
+
+            //switch (NAme)
+
+            //{
+            //    case "Ahmed":
+            //        Console.WriteLine("Hi Ahmed ");
+            //        break;
+            //    case "Omar":
+            //        Console.WriteLine("Hi Omar ");
+
+            //}
+            #endregion
+            #region EX02
+
+            //Console.WriteLine("Enter U Age");
+            //inn Age = int.Parse(Console.ReadLine());
+            //Using IF
+
+            //if (Age > 18)
+            //{
+            //    Console.WriteLine("Welcaome :");
+            //}
+            //else if (Age < 18)
+            //{
+            //    Console.WriteLine("Welcaome :");
+            //}
+            //else (Age = 18 )
+            //{
+            //    {
+            //        Console.WriteLine("Equal :");
+            //    }
+            //}
+            //Using  Switch
+            //switch (Age)
+
+            //{
+            //    case > 18:
+            //            Console.WriteLine("Welcaome :");
+            //            break;
+            //    case < 18:
+            //            Console.WriteLine("Welcaome :");
+            //        break;
+            //    default:
+            //        Console.WriteLine("eq");
+            //        break;
+            //}
+            #endregion
+
+            #region C# 7.0
+            //object Object = new object();
+            //Object = "Ahmed ";
+            //Object = 1;
+
+            //switch (Object)
+            //{
+            //    case int X when X > -12:
+
+            //        Console.WriteLine("int Object ");
+            //break;
+            //    case string X when X.Length > 5:
+
+            //        Console.WriteLine("string Object");
+            //        break ;
+            //}
+
+
+            #endregion
+
+            #region C# 8.0
+
+            //string Input = Console.ReadLine();
+            ////1
+            ////2
+            ////3
+            //string Output = Input switch 
+            //{ 
+
+            //   "1" => "Option 1",
+            //   "2" => "Option 2",
+            //   "3" => "Option 3",
+            //   _ => "Unsupport Option "
+
+            //};
+            //Console.WriteLine(  Output);
+            #endregion
+
+            #region stirng Formatatting 1,2,3 Loop
+
+            // Control Statements : 2. Loop Control Statements (For , Foreach , While , Do While )
+            //            Console.WriteLine("Hello World");
+            //1. For Loop 
+            //for (/*Statement*/ ; /*Expression*/; /*Statement*/ )
+            //{ 
+            ////Code 
+            //// Call Conv This call Code Will Repeated
+            //}
+
+
+            //for (Console.WriteLine("Hello 1"); i <10; Console.WriteLine("Hello 2");)
+            //{
+            //    Console.WriteLine("Hello World");
+            //}
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+
+            //}
+
+            //.2 Foreach 
+            //int[] Numbers = { 1, 2, 3, 4 };
+            ////for (int i = 0; i < Numbers.Length; i++)
+            ////{
+
+            ////    Console.WriteLine(Numbers[i]);
+            ////}
+
+            //foreach (int N in Numbers)
+            //{
+            //    Console.WriteLine(N);
+            //}
+
+            //While 
+
+            //while ( /*Expression*/)
+            //{
+            //    //Code
+            //    // That Code Will Repeated While The Expression Is True
+            //}
+
+            //while ( true)
+            //{
+            //    Console.WriteLine("Hallow World");
+            ////}
+            //int i = 1;
+            //while (i <= 10 )
+            //{
+            //    Console.WriteLine("Hallow World");
+            //    i++;
+            //}
+            //Do - While 
+            //do
+            //{
+            //    //Code
+            //    //That Code Will Executed First 
+            //}
+
+            //while (/*Expression*/);
+            //int i = 11;
+
+            //do
+            //{
+            //    Console.WriteLine("Hallow World");
+            //    i++;
+
+            //} while (i <= 10);
+            //int Number;
+            //bool flag;
+            //do {
+            //    Console.WriteLine("Even Number : ");
+            //    flag = int.TryParse(Console.ReadLine(), out Number);
+
+            //} while (Number % 2 != 0 || flag ==false);
+
+
+            //Console.WriteLine(Number);
+
+            #endregion
+
+
+            #region String
+
+            //  //string : Reference Type : HEAP (Class)
+            //  //Immutable type : Cant Change Its Value After Creatation 
+            //  string S01;
+            //  // Declare For Reference (Pointer) From Type 'String'
+            //  //S01 : Can Refet  To Obhect From 'String' 
+            //  //S01 : Refet To Null 
+            //  //8 Bytes Will Be Allocated At Stack For Referncr 'S01'
+            //  //8 Bytes Will Be Allocated At Heap
+
+            ////  S01 = new string("Ahmed");
+            //  S01 = "Ahmed"; //Syntex Suger
+            //  //New 
+            //  //1. Allocte The Number Of Required Bytes For The Object AT Heap
+            //  //2. Initialized The Allocated Bytes With  Default Values 
+            //  //3. Call User-defined Constryuctor Is Exists
+            //  //4. Assign The Object TO The Reference
+            //  Console.WriteLine(S01.GetHashCode()); // ID : Indicte Address
+            //  S01 = "Ali";
+            //  //1. Allocte The Number Of Required Bytes For The Object AT Heap
+            //  //2. Initialized The Allocated Bytes With  Default Values 
+            //  //3. Call User-defined Constryuctor Is Exists
+            //  //4. Assign The Object TO The Reference
+            //  Console.WriteLine(S01.GetHashCode());
+            //  S01 = "Yasmeen";
+            //  //1. Allocte The Number Of Required Bytes For The Object AT Heap
+            //  //2. Initialized The Allocated Bytes With  Default Values 
+            //  //3. Call User-defined Constryuctor Is Exists
+            //  //4. Assign The Object TO The Reference
+            //  Console.WriteLine(S01.GetHashCode());
+
+            //string Message = "Hello";
+            //Console.WriteLine("Messge :" + Message);
+            //Console.WriteLine("Messge :" + Message.GetHashCode);
+            //Console.WriteLine();
+            //Message += "Ahmed";
+            //Console.WriteLine("Messge :" + Message);
+            //Console.WriteLine("Messge :" + Message.GetHashCode);
+                     
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
